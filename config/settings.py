3b3994fe92992bf59195c3ec431d9830e1744431
alexandra kashina <kashin159@gmail.com>
2025-01-18 15:29:25 +0300

    обновление файлов

--- config/settings.py
+++ config/settings.py
@@ -199,6 +199,6 @@ if "test" in sys.argv:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
+            "NAME": BASE_DIR / "test_db_sqlite3",
         }
     }
